95871718ca6c87b8cd76407c31f6bf7c0f522030
Maksim Vasic <vasicmaksim4@gmail.com>
2026-02-22 23:51:27 +0100

    finished about page

--- portfolio/src/pages/AppAbout.tsx
+++ portfolio/src/pages/AppAbout.tsx
@@ -4,7 +4,19 @@ import AppTimeline from "@/components/app/AppTimeline.tsx";
 function AppAbout() {
     return (
         <>
-            <div className="p-4 bg-sidebar">
+            <div className="p-8 bg-sidebar">
+                <div className="px-10">
+                    <h1 className={"font-open-sans text-2xl font-bold"}>About</h1>
+                    <div className="text-xl px-8 flex items-center justify-between">
+                        <ul className="list-disc pl-8 mt-2 font-open-sans space-y-2">
+                            <li className="text-2xl">Passionate about software development and systems design</li>
+                            <li className="text-2xl">Eager to explore new ideas, concepts and technologies</li>
+                            <li className="text-2xl">Blending teamwork, leadership and innovation in every project</li>
+                            <li className="text-2xl">I enjoy turning challenges into opportunities for growth</li>
+                        </ul>
+                        <img className={"max-w-lg max-h-xl rounded-sm"} src={"src/assets/systems.jpg"}  alt={"Systems design"}/>
+                    </div>
+                </div>
                 <div className="py-4">
                     <AppTimeline />
                 </div>
--- portfolio/src/pages/AppHome.tsx
+++ portfolio/src/pages/AppHome.tsx
@@ -1,5 +1,3 @@
-
-
 function AppHome() {
     return (
         <>
